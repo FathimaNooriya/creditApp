0ffed7e2de9010bc8d9c76a9db6b1dea7c7c7fe2
FathimaNooriya <fathimanooriyakn@gmail.com>
2024-10-07 10:51:14 +0530

    udated the outstanding

--- lib/application/business_logic/transaction_controller.dart
+++ lib/application/business_logic/transaction_controller.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:moderncreditapp/application/business_logic/home_controller.dart';
 import '../../data/services/firestore_data_source.dart';
 import '../../domain/model/transaction/transaction_model.dart';
+import 'creditor_details_controller.dart';
 
 class TransactionController extends GetxController {
   final TextEditingController amountController = TextEditingController();
   final TextEditingController descriptionController = TextEditingController();
   final GlobalKey<FormState> transactionFormKey = GlobalKey<FormState>();
   final RxString transactionType = 'credit'.obs;
+  final HomeController homeController = Get.put(HomeController());
+  final creditDetailController = Get.put(CreditorDetailsController());
 
   // Add transaction function
   Future<void> addTransaction(String creditorId) async {
@@ -27,7 +31,21 @@ class TransactionController extends GetxController {
       //  String transactionId =
       await FirestoreDataSource().addTransaction(transaction);
       // transaction.id = transactionId;
-      print('Transaction added successfully with ID: transactionId');
+      print('Transaction added successfully ');
+      final creditor = await FirestoreDataSource().getCreditorById(creditorId);
+      if (transactionType.value == 'credit') {
+        await FirestoreDataSource().updateCreditorOutsnding(
+            creditorId: transaction.creditorId,
+            amount: creditor.totalOutstandingCredit + transaction.amount);
+      } else {
+        await FirestoreDataSource().updateCreditorOutsnding(
+            creditorId: transaction.creditorId,
+            amount: creditor.totalOutstandingCredit - transaction.amount);
+      }
+      print('Amount added successfully');
+      creditDetailController.fetchTransactions(creditorId: creditorId);
+      creditDetailController.fetchCreditorDetails(creditorId: creditorId);
+      homeController.getCredtList();
     } catch (e) {
       print('Failed to add transaction: $e');
     }
